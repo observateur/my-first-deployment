c5c6df5ab3e3d93efe32004e8e93ee9fd1616277
Parviz Samadov <obs@users-MacBook-Pro-2.local>
2018-09-14 11:40:27 -0700

    first commit

---
+++ loginproject/basicapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from basicapp import views
+
+
+app_name = 'basicapp'
+
+urlpatterns = [
+
+    path('regsiter/', views.register, name='register'),
+    path('user_login/',views.user_login, name='user_login')
+
+]
